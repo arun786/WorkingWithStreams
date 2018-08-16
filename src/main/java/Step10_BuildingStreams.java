5d16eb85a0588f5ba1ca4127bbd511844dc3344e
Adwiti789 <adwiti1975@gmail.com>
2018-08-16 00:48:37 -0700

    stream iterate

--- src/main/java/Step10_BuildingStreams.java
+++ src/main/java/Step10_BuildingStreams.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -13,5 +14,24 @@ public class Step10_BuildingStreams {
         Stream<String> hello = Stream.of("Hello", " Arun", " How are you");
         List<String> collect = hello.map(String::toUpperCase).collect(Collectors.toList());
         System.out.println(collect);
+
+        /**
+         * Streams from Arrays
+         */
+        int [] numbers = {2,3,4,5,6,7};
+        int sum = Arrays.stream(numbers).sum();
+        System.out.println(sum);
+
+        /**
+         * Creating an infinite stream, using Stream.iterate
+         */
+        List<Integer> infiniteStream = Stream.iterate(0, n -> n + 2).limit(10).collect(Collectors.toList());
+        System.out.println(infiniteStream);
+
+        /**
+         * fibonacci series
+         */
+        List<Integer> fibonnaci = Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]}).limit(20).map(t -> t[0]).collect(Collectors.toList());
+        System.out.println(fibonnaci);
     }
 }
